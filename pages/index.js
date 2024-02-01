4448cfc69a2fc01d66c61c7d34fde7053f9c0ccb
Carl-AndrewDizon <caydizon.student@ua.edu.ph>
2024-02-01 14:11:58 +0800

    <Hello Git World!>

--- pages/index.js
+++ pages/index.js
@@ -9,21 +9,19 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p style={{ textAlign: 'center'}}>
+        <p style={{ textAlign: 'center'}}></p>
         <p>I'm 20 years old and I live in High School Boulevard, San Fernando, Pampanga.
           My hobbies are reading and watching anime. 
           I took computer engineering because I took some interest in computer stuffs. 
           I'm still unsure of my future but I hope I can apply what I will learn in my course to my future.</p>
-        </p>
-          <p style={{ textAlign: 'center'}}>
+          <p style={{ textAlign: 'center'}}></p>
           <strong>EXPECTATIONS IN THIS SUBJECT:</strong>
-          <ul style={{ listStyle: 'disc',
-          textAlign: 'left', marginLeft:'2em'}}>
+          <ul style={{ listStyle: 'disc', textAlign: 'left', marginLeft:'2em'}}>
             <li>To understand the lessons thoroughly</li>
             <li>To be guided when I don't understand the lesson</li>
             <li>To become better at being a Computer Engineer</li>
           </ul>
-        </p>
+          <p>I successfully updated my blog!</p>
       </section>
     </Layout>
   );
